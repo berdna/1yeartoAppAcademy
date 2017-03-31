2029201547b18d9f29550353f3ebf1b08f74df79
Andre Barnes <contact@csabarnes.com>
2017-03-31 06:35:07 -0400

    Create ruby_03.rb
    Checks whether number is even or not

---
+++ HackerRank/ruby_03.rb
@@ -0,0 +1,12 @@
+    # add your code here 
+# assuming number is already defined , check whether it is even or not
+number.even?
+#or
+return  number.even?
+#or
+if number % 2 == 0
+    puts("The number is even")
+else
+    puts("The number is odd")
+end
+    
