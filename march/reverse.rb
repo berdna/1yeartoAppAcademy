67a9d2632cbef5669a0e9da8dc256ffd094ed9ba
Andre Barnes <contact@csabarnes.com>
2017-03-31 01:29:54 -0400

    Create reverse.rb

---
+++ march/reverse.rb
@@ -0,0 +1,11 @@
+# Write a method that will take a string as input, and return a new
+# string with the same letters in reverse order.
+#
+# Don't use String's reverse method; that would be too simple.
+#
+# Difficulty: easy.
+
+def reverse(String)
+  input = gets
+  input_reverse = input.split("")
+  puts(in
