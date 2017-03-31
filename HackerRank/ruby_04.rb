eb56088acf4433a029d8155207ba72588f815887
Andre Barnes <contact@csabarnes.com>
2017-03-31 06:39:48 -0400

    Create ruby_04.rb
    A in range of two variables

---
+++ HackerRank/ruby_04.rb
@@ -0,0 +1,9 @@
+Three variables a, b, and c are already defined. 
+Your task is to write code that checks whether a is within the range of b and c by calling the method range? 
+(which we have defined for you as a method for this example) on a and passing b and c as arguments.
+
+a.range?(b, c)
+
+or 
+
+return a.range?(b, c)
