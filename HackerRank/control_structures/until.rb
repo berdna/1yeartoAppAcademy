c125e261aaaf743b201d75f1639cff2e7a481c12
Andre Barnes <contact@csabarnes.com>
2017-03-31 07:18:17 -0400

    Create until.rb

---
+++ HackerRank/control_structures/until.rb
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+coder.pratice until coder oh_one? 
+
+#or
+#while not can subsititute until
+#will cause runtime errors because of non multithreading
+
+until(coder.oh_one?)
+    coder.practice
+end
