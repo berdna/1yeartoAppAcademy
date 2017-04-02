e2919b83f10b6dc2a5f44420b8f46d04a76caf64
Andre Barnes <contact@csabarnes.com>
2017-04-01 23:43:59 -0400

    Create ruby_array1.rb

---
+++ April/ruby_array1.rb
@@ -0,0 +1,7 @@
+# Initialize 3 variables here as explained in the problem statement
+#Initialize an empty array with the variable name array
+array = []
+#Initialize an array with exactly one nil element in it with the variable name array_1
+array_1 = [nil]
+#Initialize an array with exactly two elements with value 10 in it using the variable name array_2.
+array_2 = [10, 10]
